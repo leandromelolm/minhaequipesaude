2128f971d0e790c80e8bbf5947c21e869c1c2f4d
leandromelolm <leandrosiqueira.pe@gmail.com>
2026-07-14 11:18:07 -0300

    feat(sobre): pagina com informacoes da unidade

--- minhaequipesaude_frontend/src/app/app.routes.ts
+++ minhaequipesaude_frontend/src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { ProfissionaisComponent } from './features/profissionais/profissionais.component';
 import { EnderecosComponent } from './features/enderecos/enderecos.component';
 import { EquipesComponent } from './features/equipes/equipes.component';
+import { SobreComponent } from './features/sobre/sobre.component';
 
 export const routes: Routes = [
   // linha seguinte da problema em produção no firebase. a rota "" não encontra página
@@ -10,5 +11,6 @@ export const routes: Routes = [
   { path: 'equipes', component: EquipesComponent },
   { path: 'profissionais', component: ProfissionaisComponent },
   { path: 'profissionais/:equipeApelido', component: ProfissionaisComponent },
-  { path: 'busca-ruas', component: EnderecosComponent }
+  { path: 'busca-ruas', component: EnderecosComponent },
+  { path: 'sobre', component: SobreComponent }
 ];
---
+++ minhaequipesaude_frontend/src/app/features/sobre/sobre.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { EquipesComponent } from '../equipes/equipes.component';
+
+@Component({
+  selector: 'app-sobre',
+  imports: [],
+  templateUrl: './sobre.component.html',
+  styleUrl: './sobre.component.scss',
+})
+export class SobreComponent {
+
+}
