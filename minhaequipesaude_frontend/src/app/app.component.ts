f872282951528620b910a346ae085e2167c8289c
leandromelolm <leandrosiqueira.pe@gmail.com>
2026-07-19 10:32:10 -0300

    refactor(menu-desktop): separa componente menu-desktop

--- minhaequipesaude_frontend/src/app/app.component.ts
+++ minhaequipesaude_frontend/src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 import { MenuMobileComponent } from './components/menu-mobile/menu-mobile.component';
+import { MenuDesktopComponent } from './components/menu-descktop/menu-desktop.component';
 // import { NgOptimizedImage } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, RouterLink, RouterLinkActive, MenuMobileComponent],
+  imports: [RouterOutlet, MenuDesktopComponent, MenuMobileComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ minhaequipesaude_frontend/src/app/components/menu-descktop/menu-desktop.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-menu-desktop',
+  templateUrl: './menu-desktop.component.html',
+  styleUrls: ['./menu-desktop.component.css'],
+  standalone: true,
+  imports: [RouterModule]
+})
+export class MenuDesktopComponent {}
