5f58ebb184a86c3a91905ebf3a94ff7137572ead
leandromelolm <leandrosiqueira.pe@gmail.com>
2026-07-19 10:39:32 -0300

    refactor(footer): separa componente do app.component

--- minhaequipesaude_frontend/src/app/app.component.ts
+++ minhaequipesaude_frontend/src/app/app.component.ts
@@ -2,12 +2,12 @@ import { Component } from '@angular/core';
 import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 import { MenuMobileComponent } from './components/menu-mobile/menu-mobile.component';
 import { MenuDesktopComponent } from './components/menu-descktop/menu-desktop.component';
-// import { NgOptimizedImage } from '@angular/common';
+import { FooterComponent } from './components/footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, MenuDesktopComponent, MenuMobileComponent],
+  imports: [RouterOutlet, MenuDesktopComponent, MenuMobileComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
